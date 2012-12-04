b081120658f189608c73f81ad72dd915922eefca
Thomas Clavier <tom@tcweb.org>
2012-12-04 11:32:27 +0100

    missing require ...

--- manifests/vm.pp
+++ manifests/vm.pp
@@ -17,10 +17,11 @@ define puppet-lxc::vm ( $ip, $mac, $passwd, $distrib ) {
 
   exec {
     "create ${name} container": 
-      command     => "/usr/bin/lxc-create -n ${name} -t debian -- --preseed-file=/var/lib/lxc/${name}/preseed.cfg > /tmp/lxc-${name}.log 2>&1",
+      command     => "/usr/bin/lxc-create -n ${name} -t debian -B none -- --preseed-file=/var/lib/lxc/${name}/preseed.cfg > /tmp/lxc-${name}.log 2>&1",
       require     => File ["/var/lib/lxc/${name}/preseed.cfg"],
       refreshonly => false,
-      creates     => "/var/lib/lxc/${name}/config"
+      creates     => "/var/lib/lxc/${name}/config",
+      require     => Lvm::Volume["$name"];
   }
 
 
