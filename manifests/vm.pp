0f73f23495a738fdfe91a264ecc15c5ea9e37034
Thomas Clavier <tom@tcweb.org>
2012-12-04 17:57:55 +0100

    test Ins file ...

--- manifests/vm.pp
+++ manifests/vm.pp
@@ -24,7 +24,7 @@ define puppet-lxc::vm ( $ip, $mac, $passwd, $distrib ) {
   }
 
   augeas { "config ${name}":
-    lens    => "Puppet.lns",
+    lens    => "Dput.lns",
     incl    => "/var/lib/lxc/${name}/config",
     changes => [
       "set lxc.network.type veth",
